48c0fd03e9d1ccfa588a871c9e929a4cc6d6325b
Mo Zhang <mzhang@microsoft.com>
2018-10-25 15:58:08 +0200

    add functions and ignore

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flask import Flask
 
+from util import shared
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, Murat!"
+    user_name = shared.USER_NAME
+    return f"Hello, {user_name}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
